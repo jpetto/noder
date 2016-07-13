08b7ff97ba0e1821335116d8a3f57e44ced67750
Jon Petto <jonpetto@gmail.com>
2016-07-13 11:00:18 -0500

    Change response text yet again.

--- hello-world.js
+++ hello-world.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Coffee time?");
+  response.write("Coffee time? No, not quite yet. But soon...");
   response.end();
 }).listen(17959);
